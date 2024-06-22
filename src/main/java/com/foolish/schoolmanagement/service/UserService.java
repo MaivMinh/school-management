52d45bf1a654e2ec0c49389efc304ebb2e61d0cf
MaivMinh <maivanminh.se@gmail.com>
2024-06-22 22:33:03 +0700

    feat: Hoàn thành phần sử dụng Bcrypt để hashing password. Chú ý khi hàm save() trả về một User thì nó sẽ thực hiện validation User đó một lần nữa. Khi đó password và confirmPassword sẽ khác nhau, nên chúng ta sẽ tạo ra một đối tượng PasswordEncoder để kiểm tra xem hash value và plain text password có khớp hay không.

--- src/main/java/com/foolish/schoolmanagement/service/UserService.java
+++ src/main/java/com/foolish/schoolmanagement/service/UserService.java
@@ -5,23 +5,28 @@ import com.foolish.schoolmanagement.model.User;
 import com.foolish.schoolmanagement.repo.RolesRepo;
 import com.foolish.schoolmanagement.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
   private final UserRepo userRepo;
   private final RolesRepo roleRepo;
+  private final PasswordEncoder passwordEncoder;
 
   @Autowired
-  public UserService(UserRepo userRepo, RolesRepo roleRepo) {
+  public UserService(UserRepo userRepo, RolesRepo roleRepo, PasswordEncoder _passwordEncoder) {
     super();
     this.userRepo = userRepo;
     this.roleRepo = roleRepo;
+    this.passwordEncoder = _passwordEncoder;
   }
 
   public boolean createNewUser(User user) {
     Roles roles = roleRepo.getRolesByRoleName(Roles.STUDENT_ROLE);
     user.setRoles(roles);
+    // Hashing password before storing into database.
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
     user = userRepo.save(user);
     if (user != null && user.getUserId() > 0)
       return true;
--- src/main/java/com/foolish/schoolmanagement/validations/FieldsValueMatchValidator.java
+++ src/main/java/com/foolish/schoolmanagement/validations/FieldsValueMatchValidator.java
@@ -5,15 +5,18 @@ import org.springframework.beans.BeanWrapperImpl;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class FieldsValueMatchValidator implements ConstraintValidator<FieldsValueMatch, Object> {
   private String field;
   private String fieldMatch;
+  private BCryptPasswordEncoder passwordEncoder;
 
   @Override
   public void initialize(FieldsValueMatch constraintAnnotation) {
     this.field = constraintAnnotation.field();
     this.fieldMatch = constraintAnnotation.fieldMatch();
+    this.passwordEncoder = new BCryptPasswordEncoder(16);
   }
 
   @Override
@@ -23,7 +26,7 @@ public class FieldsValueMatchValidator implements ConstraintValidator<FieldsValu
     Object fieldMatchValue = new BeanWrapperImpl(value)
             .getPropertyValue(fieldMatch);
     if (fieldValue != null) {
-      return fieldValue.equals(fieldMatchValue);
+      return fieldValue.equals(fieldMatchValue) || passwordEncoder.matches((String) fieldMatchValue, (String) fieldValue);
     } else {
       return fieldMatchValue == null;
     }
