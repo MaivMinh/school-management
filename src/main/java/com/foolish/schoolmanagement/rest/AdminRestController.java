33b1490cf3cd3618bed314086a94c0e4cb8771aa
MaivMinh <maivanminh.se@gmail.com>
2024-07-25 16:36:48 +0700

    Complete a part of consuming REST service

--- src/main/java/com/foolish/schoolmanagement/rest/AdminRestController.java
+++ src/main/java/com/foolish/schoolmanagement/rest/AdminRestController.java
@@ -8,11 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
